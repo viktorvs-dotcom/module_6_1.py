c12858c86f09de8e42609d05e1642ac5c7c4e75d
viktorvs-dotcom <viktorvs1987@gmail.com>
2024-08-02 21:36:45 +0300

    Create module_6_1.py

---
+++ module_6_1.py
@@ -0,0 +1,54 @@
+class Animal:
+    def __init__(self, eat):
+        self.alive = True
+        self.fed = False
+        self.name = eat
+
+
+class Mammal(Animal):
+
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} не стал есть {food.name}")
+            fed = True
+        else:
+            print(f"{self.name} съел {food.name}")
+
+
+class Predator(Animal):
+
+    def eat(self, alive):
+        if alive.edible:
+            print(f"{self.name} съел {alive.name}")
+            fed = True
+        else:
+            print(f"{self.name} не стал есть {alive.name}")
+
+
+class Plant:
+    def __init__(self, name):
+        self.edible = False
+        self.name = name
+
+
+class Flower(Plant):
+    pass
+
+
+class Fruit(Plant):
+    edible = True
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a2.fed)
+print(a1.alive)
